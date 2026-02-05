345dd1831b8aa1f9dfa324475f8f2568782d713c
OtavioLara <othaviolara@gmail.com>
2026-02-05 17:11:01 -0300

    Creating menu interations Creating ABC class Creating Factory Level 1 bg Implementation: parallax

--- code/Menu.py
+++ code/Menu.py
@@ -4,7 +4,7 @@ from pygame.font import Font
 from pygame.rect import Rect
 from pygame.surface import Surface
 
-from code.Const import WIN_WIDTH, WIN_HEIGHT, COLOR_ORANGE, MENU_OPTION, C_WHITE
+from code.Const import WIN_WIDTH, WIN_HEIGHT, COLOR_ORANGE, MENU_OPTION, C_WHITE, COLOR_YELLOW
 
 
 class Menu:
@@ -15,23 +15,56 @@ class Menu:
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
+        menu_option = 0
         pygame.mixer.music.load("./asset/Menu.mp3")
         pygame.mixer.music.play(-1)
+
+        clock = pygame.time.Clock()  # cria o relógio
+        delay = 200  # tempo mínimo entre movimentos
+        last_move = 0  # armazena o último movimento
+
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(text_size=50, text="Mountain", text_color=C_WHITE, text_center_pos=((WIN_WIDTH /2),70))
-            self.menu_text(text_size=50, text="Shooter", text_color=C_WHITE, text_center_pos=((WIN_WIDTH /2),120))
+            self.menu_text(text_size=50, text="Mountain", text_color=COLOR_ORANGE, text_center_pos=((WIN_WIDTH /2),70))
+            self.menu_text(text_size=50, text="Shooter", text_color=COLOR_ORANGE, text_center_pos=((WIN_WIDTH /2),120))
 
             for i in range(len(MENU_OPTION)):
-                self.menu_text(text_size=20, text=MENU_OPTION[i], text_color=C_WHITE, text_center_pos=((WIN_WIDTH /2),200 + 25 * i))
-
-            pygame.display.flip()
+                if i == menu_option:
+                    self.menu_text(text_size=20, text=MENU_OPTION[i], text_color=COLOR_YELLOW,
+                                   text_center_pos=((WIN_WIDTH / 2), 200 + 25 * i))
+                else:
+                    self.menu_text(text_size=20, text=MENU_OPTION[i], text_color=C_WHITE, text_center_pos=((WIN_WIDTH /2),200 + 25 * i))
 
             # Check for all events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit() # close Window
                     quit() # end game
+                if event.type == pygame.KEYDOWN:  # somente KEYDOWN tem "key"
+                    if event.key == pygame.K_RETURN:
+                        return MENU_OPTION[menu_option]
+
+            keys = pygame.key.get_pressed()
+            current_time = pygame.time.get_ticks()
+
+            if keys[pygame.K_DOWN]:
+                if current_time - last_move > delay:
+                    if menu_option < len(MENU_OPTION) - 1:
+                        menu_option += 1
+                    else:
+                        menu_option = 0
+                    last_move = current_time
+
+            if keys[pygame.K_UP]:
+                if current_time - last_move > delay:
+                    if menu_option > 0:
+                        menu_option -= 1
+                    else:
+                        menu_option = len(MENU_OPTION) - 1
+                    last_move = current_time
+
+            pygame.display.flip()
+            clock.tick(60)  # 60 FPS
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font = pygame.font.Font("./asset/font.ttf", text_size)
