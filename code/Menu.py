70de5fb96f04f136f92af1a8469d3f127eb1709d
OtavioLara <othaviolara@gmail.com>
2026-02-05 11:45:48 -0300

    Creating asset folder Inserting all assets Creating the menu: background, sound and fonts crating the const file

--- code/Menu.py
+++ code/Menu.py
@@ -1,10 +1,42 @@
 
+import pygame.image
+from pygame.font import Font
+from pygame.rect import Rect
+from pygame.surface import Surface
+
+from code.Const import WIN_WIDTH, WIN_HEIGHT, COLOR_ORANGE, MENU_OPTION, C_WHITE
 
 
 class Menu:
 
     def __init__(self, window):
         self.window = window
+        self.surf = pygame.image.load("./asset/MenuBg.png")
+        self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
-        pass
+        pygame.mixer.music.load("./asset/Menu.mp3")
+        pygame.mixer.music.play(-1)
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(text_size=50, text="Mountain", text_color=COLOR_ORANGE, text_center_pos=((WIN_WIDTH /2),70))
+            self.menu_text(text_size=50, text="Shooter", text_color=COLOR_ORANGE, text_center_pos=((WIN_WIDTH /2),120))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(text_size=20, text=MENU_OPTION[i], text_color=C_WHITE, text_center_pos=((WIN_WIDTH /2),200 + 25 * i))
+
+            pygame.display.flip()
+
+            # Check for all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit() # close Window
+                    quit() # end game
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font = pygame.font.Font("./asset/font.ttf", text_size)
+        text_surf = text_font.render(text, True, text_color)
+        text_rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(text_surf, text_rect)
+
+
